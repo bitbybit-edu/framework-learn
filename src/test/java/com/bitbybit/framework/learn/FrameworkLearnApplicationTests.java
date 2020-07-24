b5f6453a6a8c4c7264ebe597fdb493daf4bf03d6
liulin <liulin4375@dingtalk.com>
2020-07-24 21:00:45 +0800

    framework learn

--- src/test/java/com/bitbybit/framework/learn/FrameworkLearnApplicationTests.java
+++ src/test/java/com/bitbybit/framework/learn/FrameworkLearnApplicationTests.java
@@ -1,22 +1,30 @@
 package com.bitbybit.framework.learn;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class FrameworkLearnApplicationTests {
 
-	@Autowired
-	ApplicationContext applicationContext;
+    private static final Logger logger = LoggerFactory.getLogger(FrameworkLearnApplicationTests.class);
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    ApplicationContext applicationContext;
 
-	@Test
-	void applicationContextTest() {
-		System.out.println(applicationContext);
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void applicationContextTest() {
+        TestComponent bean = applicationContext.getBean(TestComponent.class);
+        Object testComponent = applicationContext.getBean("testComponent");
+        TestComponent testComponent1 = applicationContext.getBean("testComponent", TestComponent.class);
+        logger.info("{} {} {}", bean, testComponent, testComponent1);
+    }
 }
