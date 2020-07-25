041546c2b322507ee004570c7fab331b1d1cd2b2
liulin <liulin4375@dingtalk.com>
2020-07-25 10:35:51 +0800

    测试getBean

--- src/main/java/com/bitbybit/framework/learn/component/TestApplicationGetBean.java
+++ src/main/java/com/bitbybit/framework/learn/component/TestApplicationGetBean.java
@@ -1,11 +1,14 @@
 package com.bitbybit.framework.learn.component;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TestComponent {
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class TestApplicationGetBean {
 
     @Autowired
     ApplicationContext applicationContext;
--- src/test/java/com/bitbybit/framework/learn/FrameworkLearnApplicationTests.java
+++ src/test/java/com/bitbybit/framework/learn/FrameworkLearnApplicationTests.java
@@ -1,11 +1,11 @@
 package com.bitbybit.framework.learn;
 
+import com.bitbybit.framework.learn.component.TestApplicationGetBean;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestComponent;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
@@ -22,9 +22,33 @@ class FrameworkLearnApplicationTests {
 
     @Test
     void applicationContextTest() {
-        TestComponent bean = applicationContext.getBean(TestComponent.class);
-        Object testComponent = applicationContext.getBean("testComponent");
-        TestComponent testComponent1 = applicationContext.getBean("testComponent", TestComponent.class);
-        logger.info("{} {} {}", bean, testComponent, testComponent1);
+        try {
+            TestApplicationGetBean bean = applicationContext.getBean(TestApplicationGetBean.class);
+            logger.info("getBean(TestApplicationGetBean.class) = {}", bean);
+        } catch (Exception e) {
+            logger.error("getBean(TestApplicationGetBean.class) fail", e);
+        }
+
+        try {
+            TestApplicationGetBean bean = applicationContext.getBean("testApplicationGetBean", TestApplicationGetBean.class);
+            logger.info("getBean(\"testApplicationGetBean\", TestApplicationGetBean.class) = {}", bean);
+        } catch (Exception e) {
+            logger.error("getBean(\"testApplicationGetBean\", TestApplicationGetBean.class) fail", e);
+        }
+
+        try {
+            Object bean = applicationContext.getBean("testApplicationGetBean");
+            logger.info("getBean(\"testApplicationGetBean\") = {}", bean);
+        } catch (Exception e) {
+            logger.error("getBean(\"testApplicationGetBean\") fail", e);
+        }
+
+        try {
+            Object bean = applicationContext.getBean("testApplicationGetBeanC");
+            logger.info("getBean(\"testApplicationGetBeanC\") = {}", bean);
+        } catch (Exception e) {
+            logger.error("getBean(\"testApplicationGetBeanC\") fail", e);
+        }
+
     }
 }
