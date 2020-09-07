246b646109ed7aabd3ebdcc5f1c44a2cd00f904c
liulin <liulin4375@dingtalk.com>
2020-09-07 08:42:57 +0800

    注解实现事件监听（异步）   成功

--- src/main/java/com/bitbybit/framework/learn/event/Main02Async.java
+++ src/main/java/com/bitbybit/framework/learn/event/Main02Async.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
-//@EnableAsync
+@EnableAsync
 @Configuration
 @ComponentScan("com.bitbybit.framework.learn.event")
 public class Main02Async {
