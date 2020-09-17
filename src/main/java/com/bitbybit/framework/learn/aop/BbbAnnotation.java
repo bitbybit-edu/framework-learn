52bd436ff59a623fc3c1fb00b7e02670a4f0b77e
liulin <liulin4375@dingtalk.com>
2020-09-17 20:53:02 +0800

    @Aspect can not pass

---
+++ src/main/java/com/bitbybit/framework/learn/aop/BbbAnnotation.java
@@ -0,0 +1,14 @@
+package com.bitbybit.framework.learn.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author liulin
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface BbbAnnotation {
+}
